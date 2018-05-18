5bfb4f23166de5828dbde9e827a9edb4ce169d7a
Esayas Gebre <dave183278@gmail.com>
2018-05-18 16:26:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var  theFunk = funkyFunction();
 
-function() {
-  return function() {
+function funkyFunction(){
+  
     return "FUNKY!"
   }
-}
+
